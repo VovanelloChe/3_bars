41b6e52cf438cb582146f7f94d657b4309811668
VovanelloChe <45932515+VovanelloChe@users.noreply.github.com>
2019-02-25 22:22:59 +0300

    Add files via upload

--- bars.py
+++ bars.py
@@ -1,21 +1,34 @@
-import json
+import data_loaders
+import math
+from collections import namedtuple
 
+barsjson = data_loaders.load_from_json("bars.json")
 
-def load_data(filepath):
-    pass
+latig = float(input('lati = '))
 
+longig = float(input('longi = '))
 
-def get_biggest_bar(data):
-    pass
+dist_min = 10e5
 
+# функция вычисления расстояния от пользователя до текущего бара
+def calcDistance(coord, lati, longi):
+    return math.pow((coord[0] - lati)**2.0 + (coord[1] - longi)**2.0, 0.5)
 
-def get_smallest_bar(data):
-    pass
+#
+lbars = barsjson.get('features')
 
+# Объявление именованных кортежей
+BarDist = namedtuple('BarDist', ['dist', 'Name'])
+BarCount = namedtuple('BarDist', ['sCount', 'Name'])
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+# Заполнение именованных кортежей
+lBarDist = list(map(lambda bar: BarDist(dist = calcDistance(bar.get('geometry').get('coordinates'), latig, longig),
+Name = bar.get('properties').get('Attributes').get('Name')), lbars))
 
+lBarCount = list(map(lambda bar: BarCount(sCount = bar.get('properties').get('Attributes').get('SeatsCount'),
+Name = bar.get('properties').get('Attributes').get('Name')), lbars))
 
-if __name__ == '__main__':
-    pass
+# Вывод результата
+print ('Бар с наименьшим количеством мест: ', min(lBarCount).Name)
+print ('Бар с наибольшим количеством мест: ', max(lBarCount).Name)
+print ('Ближайший бар: ', min(lBarDist).Name)
