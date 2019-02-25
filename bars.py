41f2c06aef1ce2b17603e83e668d4f790647c762
VovanelloChe <45932515+VovanelloChe@users.noreply.github.com>
2019-02-25 22:54:05 +0300

    Исправлены ошибки

--- bars.py
+++ bars.py
@@ -2,33 +2,33 @@ import data_loaders
 import math
 from collections import namedtuple
 
-barsjson = data_loaders.load_from_json("bars.json")
+bars_json = data_loaders.load_from_json("bars.json")
 latig = float(input('lati = '))
 longig = float(input('longi = '))
 
 
 # функция вычисления расстояния от пользователя до текущего бара
-def calcDistance(coord, lati, longi):
+def calc_distance(coord, lati, longi):
     return math.pow((coord[0] - lati)**2.0 + (coord[1] - longi)**2.0, 0.5)
 
 
-lbars = barsjson.get('features')
+l_bars = bars_json.get('features')
 
 # Объявление именованных кортежей
-BarDist = namedtuple('BarDist', ['dist', 'Name'])
-BarCount = namedtuple('BarDist', ['sCount', 'Name'])
+bar_dist = namedtuple('BarDist', ['dist', 'Name'])
+bar_count = namedtuple('BarDist', ['sCount', 'Name'])
 
 # Заполнение именованных кортежей
-lBarDist = list(map(lambda bar: BarDist(dist=calcDistance(bar.get('geometry')
+l_bar_dist = list(map(lambda bar: bar_dist(dist=calc_distance(bar.get('geometry')
                     .get('coordinates'), latig, longig),
                     Name=bar.get('properties').get('Attributes').
-                    get('Name')), lbars))
-lBarCount = list(map(lambda bar: BarCount(sCount=bar.get('properties')
+                    get('Name')), l_bars))
+l_bar_count = list(map(lambda bar: bar_count(sCount=bar.get('properties')
                      .get('Attributes').get('SeatsCount'),
                      Name=bar.get('properties').get('Attributes')
-                     .get('Name')), lbars))
+                     .get('Name')), l_bars))
 
 # Вывод результата
-print('Бар с наименьшим количеством мест: ', min(lBarCount).Name)
-print('Бар с наибольшим количеством мест: ', max(lBarCount).Name)
-print('Ближайший бар: ', min(lBarDist).Name)
+print('Бар с наименьшим количеством мест: ', min(l_bar_count).Name)
+print('Бар с наибольшим количеством мест: ', max(l_bar_count).Name)
+print('Ближайший бар: ', min(l_bar_dist).Name)
